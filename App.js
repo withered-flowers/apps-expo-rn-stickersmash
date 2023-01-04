635493aedf0918f97ffb0f2391d4cef3fd7b189c
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-01-04 16:38:07 +0700

    feat: adding Screenshot handler

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
+import * as MediaLibrary from "expo-media-library";
 import * as ImagePicker from "expo-image-picker";
 
 import Button from "./components/Button";
@@ -15,10 +17,14 @@ import ImageViewer from "./components/ImageViewer";
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-  const [pickedEmoji, setPickedEmoji] = useState(null);
+  const imageRef = useRef();
+
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const [status, requestPermissions] = MediaLibrary.usePermissions();
 
   const onResetHandler = () => {
     setShowAppOptions(false);
@@ -32,7 +38,22 @@ export default function App() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsyncHandler = () => {};
+  const onSaveImageAsyncHandler = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+
+      if (localUri) {
+        alert("saved !");
+      }
+    } catch (err) {
+      console.log(e);
+    }
+  };
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -48,16 +69,22 @@ export default function App() {
     }
   };
 
+  if (status === null) {
+    requestPermissions();
+  }
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer
-          placeholderImageSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
-        {pickedEmoji !== null ? (
-          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-        ) : null}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+          />
+          {pickedEmoji !== null ? (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          ) : null}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
